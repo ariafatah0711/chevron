2f1ef56643cb89558f02de3d8ca4b853806a7822
ariafatah0711 <ariafatah0711@gmail.com>
2025-08-13 13:03:41 +0700

    fixed: load extension with remove vue single file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+// import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
+  // plugins: [react(), viteSingleFile()],
   base: ''
 })
